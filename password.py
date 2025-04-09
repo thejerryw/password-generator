2b0b9b254106edf755231fd6d0fe713a8f091ceb
Jerry Wang <68243688+thejerryw@users.noreply.github.com>
2025-04-08 21:27:26 -0700

    Add files via upload
    initial password.py

---
+++ password.py
@@ -0,0 +1,5 @@
+import random 
+password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+{}:" # random password will contain following characters 
+length_password = int(input("Enter the length of the password: ")) # length is input length
+a = "".join(random.sample(password, length_password)) # randomly takes characters from password and joins together in length of length_password
+print(f"Your password is {a}")
