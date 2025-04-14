887c16b186e7817c128f6dafddddae60b2563eaf
Jerry Wang <68243688+thejerryw@users.noreply.github.com>
2025-04-13 18:34:58 -0700

    added opening comment!

--- password.py
+++ password.py
@@ -1,3 +1,5 @@
+# password.py - takes in user input length, and generates random password of that length
+
 import random 
 password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+{}:" # random password will contain following characters 
 length_password = int(input("Enter the length of the password: ")) # length is input length
